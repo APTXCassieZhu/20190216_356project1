85f90a31297c1a1cbde63a729956b65f16e59ab0
stone <enrui.shi@stonybrook.edu>
2019-02-17 19:44:01 -0500

    add check win

--- html/ttt/play.php
+++ html/ttt/play.php
@@ -1,11 +1,47 @@
 <?php
+    $grid = $_POST["grid"];
+    $winner = "";
+
+    function Check(){
+        global $grid,$winner;
+        for($x = 0; $x < 3; $x++){
+            if($grid[strval($x+0)] = "X" && $grid[strval($x+3)] = "X" && $grid[strval($x+6)] = "X"){
+                return "X";
+            }
+            if($grid[strval($x*3+0)] = "X" && $grid[strval($x*3+1)] = "X" && $grid[strval($x*3+2)] = "X"){
+                return "X";
+            }
+            if($grid[strval($x+0)] = "O" && $grid[strval($x+3)] = "O" && $grid[strval($x+6)] = "O"){
+                return "O";
+            }
+            if($grid[strval($x*3+0)] = "O" && $grid[strval($x*3+1)] = "O" && $grid[strval($x*3+2)] = "O"){
+                return "O";
+            }
+        }
+        if($grid['0'] = "X" && $grid['4'] = "X" && $grid['8'] = "X"){
+            return "X";
+        }
+        if($grid['2'] = "X" && $grid['4'] = "X" && $grid['6'] = "X"){
+            return "X";
+        }
+        if($grid['0'] = "O" && $grid['4'] = "O" && $grid['8'] = "O"){
+            return "O";
+        }
+        if($grid['2'] = "O" && $grid['4'] = "O" && $grid['6'] = "O"){
+            return "O";
+        }
+        return "";
+    }
+
+    
     function Move() {
+        global $grid,$winner;
         if($grid['4'] = " "){
             $grid['4'] = "O";
         }else{
             for($x = 0; $x < 3; $x++){
-                for($y = 0; $y < 3; $y++){
-                    
+                if($grid['0'] = "X" && $grid['1'] = "X"){
+
                 }
             }
         }
@@ -13,9 +49,8 @@
 
 
     //header('Access-Control-Allow-Origin:*');//注意！跨域要加这个头 上面那个没有
-    $grid = $_POST["grid"];
-
+    $winner = check();
     $myjson -> grid = $grid;
-    $myjson -> winner = "me";
+    $myjson -> winner = $winner;
     echo json_encode($myjson);
 ?>
