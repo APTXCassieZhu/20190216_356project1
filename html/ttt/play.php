e50498b38f6ea034515b15cacec4d26a1a6256ff
stone <enrui.shi@stonybrook.edu>
2019-02-17 19:10:42 -0500

    1

--- html/ttt/play.php
+++ html/ttt/play.php
@@ -1,8 +1,21 @@
 <?php
+    function Move() {
+        if($grid['4'] = " "){
+            $grid['4'] = "O";
+        }else{
+            for($x = 0; $x < 3; $x++){
+                for($y = 0; $y < 3; $y++){
+                    
+                }
+            }
+        }
+    }
+
+
     //header('Access-Control-Allow-Origin:*');//注意！跨域要加这个头 上面那个没有
-    $gird = json_decode($_POST["grid"],true);
-    $gird['8'] = "O";
-    $myjson -> gird = $grid;
+    $grid = $_POST["grid"];
+
+    $myjson -> grid = $grid;
     $myjson -> winner = "me";
-    echo json_encode($myjson); 
+    echo json_encode($myjson);
 ?>
