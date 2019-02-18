2c3ddf755dab07b7bdb8bf9a40569602dc63eb58
stone <enrui.shi@stonybrook.edu>
2019-02-17 19:56:46 -0500

    1

--- html/ttt/play.php
+++ html/ttt/play.php
@@ -5,29 +5,29 @@
     function Check(){
         global $grid,$winner;
         for($x = 0; $x < 3; $x++){
-            if($grid[strval($x+0)] = "X" && $grid[strval($x+3)] = "X" && $grid[strval($x+6)] = "X"){
+            if($grid[strval($x+0)] == "X" && $grid[strval($x+3)] == "X" && $grid[strval($x+6)] == "X"){
                 return "X";
             }
-            if($grid[strval($x*3+0)] = "X" && $grid[strval($x*3+1)] = "X" && $grid[strval($x*3+2)] = "X"){
+            if($grid[strval($x*3+0)] == "X" && $grid[strval($x*3+1)] == "X" && $grid[strval($x*3+2)] == "X"){
                 return "X";
             }
-            if($grid[strval($x+0)] = "O" && $grid[strval($x+3)] = "O" && $grid[strval($x+6)] = "O"){
+            if($grid[strval($x+0)] == "O" && $grid[strval($x+3)] == "O" && $grid[strval($x+6)] == "O"){
                 return "O";
             }
-            if($grid[strval($x*3+0)] = "O" && $grid[strval($x*3+1)] = "O" && $grid[strval($x*3+2)] = "O"){
+            if($grid[strval($x*3+0)] == "O" && $grid[strval($x*3+1)] == "O" && $grid[strval($x*3+2)] == "O"){
                 return "O";
             }
         }
-        if($grid['0'] = "X" && $grid['4'] = "X" && $grid['8'] = "X"){
+        if($grid['0'] == "X" && $grid['4'] == "X" && $grid['8'] == "X"){
             return "X";
         }
-        if($grid['2'] = "X" && $grid['4'] = "X" && $grid['6'] = "X"){
+        if($grid['2'] == "X" && $grid['4'] == "X" && $grid['6'] == "X"){
             return "X";
         }
-        if($grid['0'] = "O" && $grid['4'] = "O" && $grid['8'] = "O"){
+        if($grid['0'] == "O" && $grid['4'] == "O" && $grid['8'] == "O"){
             return "O";
         }
-        if($grid['2'] = "O" && $grid['4'] = "O" && $grid['6'] = "O"){
+        if($grid['2'] == "O" && $grid['4'] == "O" && $grid['6'] == "O"){
             return "O";
         }
         return "";
@@ -36,11 +36,11 @@
     
     function Move() {
         global $grid,$winner;
-        if($grid['4'] = " "){
-            $grid['4'] = "O";
+        if($grid['4'] == " "){
+            $grid['4'] == "O";
         }else{
             for($x = 0; $x < 3; $x++){
-                if($grid['0'] = "X" && $grid['1'] = "X"){
+                if($grid['0'] == "X" && $grid['1'] == "X"){
 
                 }
             }
