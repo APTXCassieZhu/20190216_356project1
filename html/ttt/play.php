f0c4a482e6de27d911c39619d91c4bb76258209e
stone <enrui.shi@stonybrook.edu>
2019-02-17 20:57:56 -0500

    1

--- html/ttt/play.php
+++ html/ttt/play.php
@@ -35,14 +35,14 @@
 
     
 
-    function Move() {
+    function Move($grid) {
         global $grid;
         if($grid['4'] == " "){
             $grid['4'] == "O";
-            return "";
+            return $gridl;
         }else if ($grid['4'] == "X" && $grid['0']== " "){
             $grid['0'] == "O";
-            return "";
+            return $gridl;
         }else{
             for($x = 0; $x < 3; $x++){
                 //[] 
@@ -50,36 +50,36 @@
                 //X
                 if($grid[strval($x+0)] == " " && $grid[strval($x+3)] == "X" &&$grid[strval($x+6)] == "X"){
                     $grid[strval($x+0)] = "O";
-                    return "";
+                    return $gridl;
                 }
                 //X 
                 //[] 
                 //X
                 if($grid[strval($x+0)] == "X" && $grid[strval($x+3)] == " " &&$grid[strval($x+6)] == "X"){
                     $grid[strval($x+3)] = "O";
-                    return "";
+                    return $gridl;
                 }
                 //X 
                 //X 
                 //[]
                 if($grid[strval($x+0)] == "X" && $grid[strval($x+3)] == "X" &&$grid[strval($x+6)] == " "){
                     $grid[strval($x+6)] = "O";
-                    return "";
+                    return $gridl;
                 }
                 //[] X X
                 if($grid[strval($x*3+0)] == " " && $grid[strval($x*3+1)] == "X" &&$grid[strval($x*3+2)] == "X"){
                     $grid[strval($x*3+0)] = "O";
-                    return "";
+                    return $gridl;
                 }
                 //X [] X
                 if($grid[strval($x*3+0)] == "X" && $grid[strval($x*3+1)] == " " &&$grid[strval($x*3+2)] == "X"){
                     $grid[strval($x*3+1)] = "O";
-                    return "";
+                    return $gridl;
                 }
                 //X X []
                 if($grid[strval($x*3+0)] == "X" && $grid[strval($x*3+1)] == "X" &&$grid[strval($x*3+2)] == " "){
                     $grid[strval($x*3+2)] = "O";
-                    return "";
+                    return $gridl;
                 }
             }
             //X
@@ -87,48 +87,48 @@
             //  []
             if($grid['0'] == "X" && $grid['4'] == "X" &&$grid['8'] == " "){
                 $grid[8] = "O";
-                return "";
+                return $gridl;
             }
             //X
             // []
             //   X
             if($grid['0'] == "X" && $grid['4'] == " " &&$grid['8'] == "X"){
                 $grid[4] = "O";
-                return "";
+                return $gridl;
             }
             //[]
             //  X
             //   X
             if($grid['0'] == " " && $grid['4'] == "X" &&$grid['8'] == "X"){
                 $grid[0] = "O";
-                return "";
+                return $gridl;
             }
             //    X
             //  X
             //[]  
             if($grid['2'] == "X" && $grid['4'] == "X" &&$grid['6'] == " "){
                 $grid[6] = "O";
-                return "";
+                return $gridl;
             }
             //   X
             // []
             //X  
             if($grid['2'] == "X" && $grid['4'] == " " &&$grid['6'] == "X"){
                 $grid[4] = "O";
-                return "";
+                return $gridl;
             }
             //  []
             // X
             //X  
             if($grid['2'] == " " && $grid['4'] == "X" &&$grid['6'] == "X"){
                 $grid[2] = "O";
-                return "";
+                return $gridl;
             }
         }
         for($x=0;$x<9;$x++){
             if($grid[strval($x)] == " "){
                 grid[strval($x)] == "O";
-                return "";
+                return $gridl;
             }
         }
     }
@@ -137,7 +137,7 @@
     //header('Access-Control-Allow-Origin:*');//注意！跨域要加这个头 上面那个没有
     $winner = check();
     if($winner == ""){
-        Move();
+        Move($grid);
     }
     $myjson -> grid = $grid;
     $myjson -> winner = $winner;
